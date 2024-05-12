d0bd50ff6c82dc6b335da231f9d683f3b2530976
sabrinacouto <sabrinacoutolima@gmail.com>
2024-05-12 20:47:06 -0300

    ✨ feat: criando componente Header e adicionando rotas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,46 @@
 import Image from "next/image";
+import Mascotes from "@/components/Mascotes/mascotes";
+import mascoteHome from "../../public/assets/mascotehome.png"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section id="home">
+    <div className="section flex justify-center">
+      <div className="flex items-center justify-center  flex-col sm:flex-row w-full shadow-xl inset-y-3">
+        <div className="text">
+          <h1 className="gradient text-2xl sm:text-5xl max-w-[600px] w-full">
+            Introducing Salesforce Accessibility
+          </h1>
+         <a href="#acessibilidade"> <button type="button" naria-label="Clique aqui pra saber mais" className="bg-[#194750] w-[111px] h-[41px] rounded-md text-white mt-10">Saiba mais</button></a>
+          <div>
+            <p className="text-lg max-w-[700px] mt-5 mr-2 w-full py-3 hidden sm:block">
+              Proporcione uma experiência inclusiva e acessível a todos os usuários!
+            </p>
+          </div>
+          <div>
+            <p className=" text-lg max-w-[700px] w-full mr-2 hidden sm:block">
+              Nosso site apresenta um inovador Menu de Acessibilidade, permitindo que você ajuste o tamanho da fonte, escolha o idioma da página, ative leitores de tela, utilize opções de teclado e desfrute de recursos em linguagem de sinais.
+            </p>
+          </div>
+          <div>
+            <p className=" text-lg max-w-[700px] hidden sm:block w-full py-3">
+              Valorizamos a diversidade e buscamos tornar a sua navegação mais fácil, independente das suas necessidades.
+            </p>
+          </div>
+        </div>
+        <div className="imageCustomer">
+          <Image
+            src={mascoteHome}
+            alt="Elefante segurando um computador."
+            className="max-w-[300px] sm:max-w-[400px] w-full"
+          />
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
+    <div className="py-[100px]">
+      <Mascotes />
+    </div>
+  </section>
+   
   );
 }
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,113 @@
+'use client'
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FiMenu } from "react-icons/fi";
+import { IoCloseOutline } from "react-icons/io5";
+import logo from "../../../public/assets/logo.png";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoSearchSharp } from "react-icons/io5";
+import { FaUser } from "react-icons/fa";
+
+const Header: React.FC = () => {
+  const [isSideMenuOpen, setMenu] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
+
+  return (
+
+      <main>
+        <nav className="flex justify-between bg-[#0077B6] px-8 items-center py-6">
+          <div className="flex items-center gap-5">
+           
+              <FiMenu
+                onClick={() => setMenu(true)}
+                className="text-4xl text-white cursor-pointer lg:hidden"
+              />
+              <Link href="/">
+                <Image src={logo} className="h-[4rem] w-[5rem] md:h-[3rem] md:w-[4rem] mr-2 ml-1" alt="Uma nuvem branca com o nome da Salesforce em azul" />
+              </Link>
+         
+            <ul className="flex gap-5">
+           <li><Link href="/produtos" className="hidden lg:block text-white transition duration-500 hover:text-blue-300 transform hover:scale-110 ">Produtos</Link></li> 
+            <li><Link href="/empresa" className="hidden  lg:block text-white transition duration-500  hover:text-blue-300 transform hover:scale-110 ">Empresa</Link></li>
+            <li><Link href="/customer" className="hidden  lg:block text-white transition duration-500  hover:text-blue-300 transform hover:scale-110  ">Customer</Link></li>
+           <li><Link href="/acessibilidade" className="hidden lg:block text-white transition duration-500 hover:text-blue-300 transform hover:scale-110 ">Acessibilidade</Link></li> 
+            <li><Link href="/desenvolvedores" className="hidden lg:block text-white transition duration-500  hover:text-blue-300 transform hover:scale-110">Desenvolvedores</Link></li>
+            </ul>
+          </div>
+          
+
+          <div
+            className={` fixed h-full w-screen lg:hidden bg-white/50 backdrop-blur-sm top-0  right-0 ${
+              isSideMenuOpen ? "translate-x-0" : "-translate-x-full"
+            } transition-all`}
+          >
+            <section className="text-white bg-[#0077B6] flex-col left-0 top-0 h-screen p-8 gap-6 z-50 w-56 flex">
+              <IoCloseOutline
+                onClick={() => setMenu(false)}
+                className=" mb-8 text-2xl cursor-pointer"
+              />
+              <Link href="/produtos" className="font-bold">Produtos</Link>
+              <Link href="/empresa" className="font-bold ">Empresa</Link>
+              <Link href="/customer" className="font-bold">Customer</Link>
+              <Link href="/acessibilidade" className="font-bold ">Acessibilidade</Link>
+              <Link href="/desenvolvedores" className="font-bold ">Desenvolvedores</Link>
+              <hr className="" />
+              <div className='text-white'>
+                <p className="text-xs ml-5 mb-2 font-bold">Contato:</p>
+                <div className=" flex text-sm">
+                  <FaPhoneAlt className="h-3 mr-2 mt-1" />
+                  <a href="">0800 891 1887</a>
+                </div>
+              </div>
+            </section>
+          </div>
+
+          {!showSearch && (
+            <div className='text-white'>
+              <p className="menu-item text-xs hidden md:block ml-5 mb-1">Entre em contato</p>
+              <div className=" hidden md:flex text-sm">
+                <FaPhoneAlt className="h-3 mr-2 mt-1" />
+                <a href="">0800 891 1887</a>
+              </div>
+            </div>
+          )}
+
+          <section className="flex items-center gap-4">
+            <div className="flex items-center justify-center w-full">
+              <button type ="button" id="search"  title = "Barra de pesquisa" onClick={() => setShowSearch(!showSearch)} >
+                <IoSearchSharp className="text-xl rounded-2xl ml-3 mt-1 text-white transition duration-300 hover:bg-blue-300 bg-opacity-50 ransform hover:scale-125" />
+              </button>
+              {showSearch && <input type="search" placeholder="Encontre aqui" className=" bg-white focus:outline-none rounded text-xs ml-2 md:ml-7 py-2 pr-[2rem] " />}
+            </div>
+            {showSearch && <div className=" bg-white mt-2"></div>}
+
+            {!showSearch && (
+              <>
+                <Link href="/login">
+                  <FaUser title="Login" className="text-base fill-white hover:fill-blue-950 rounded cursor-pointer" />
+                </Link>
+                <p className="text-white font-bold">|</p>
+                <Link href="/cadastro">
+                  <button type="button" className=" w-[4rem] h-auto md:w-[10rem] md:h-10  rounded-lg border-lg  bg-blue-500 hover:bg-blue-950 text-white shadow-2xl  ">
+                 Teste grátis
+                  </button>
+                </Link>
+              </>
+            )}
+          </section>
+
+
+        </nav>
+        <hr className="" />
+     
+        
+      </main>
+
+ 
+
+
+  );
+};
+
+export default Header;
