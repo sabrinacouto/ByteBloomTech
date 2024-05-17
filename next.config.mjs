11c3aae994157fe520903786f189fdef00f9869c
sabrinacouto <sabrinacoutolima@gmail.com>
2024-05-16 23:43:45 -0300

    ♻️ refactor: modificando next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
-const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    config.resolve.extensions.push('.js', '.jsx', '.ts', '.tsx');
-    return config;
-  },
-};
 
-module.exports = nextConfig;
+    }    
+
+export default nextConfig;
