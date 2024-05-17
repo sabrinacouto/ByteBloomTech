e9536068a80e734c812621e25ab5cca2f023eb35
sabrinacouto <sabrinacoutolima@gmail.com>
2024-05-16 23:38:05 -0300

    ⬆️ build: atualizando dependências

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-    }    
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    config.resolve.extensions.push('.js', '.jsx', '.ts', '.tsx');
+    return config;
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
