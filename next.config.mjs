d46edff60b0d9c096ccddd5a48e6e236ba91c536
sabrinacouto <sabrinacoutolima@gmail.com>
2024-05-17 00:13:35 -0300

    🐛 fix: modificando arquivo next.config para realização de deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
+import path from 'path';
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    }    
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
 
 export default nextConfig;
